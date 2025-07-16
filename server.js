ea6f287b08f180379870f6ca04721383e79b5d2c
AdiGaikwad <adityagaikwad1305@gmail.com>
2025-07-16 10:55:54 +0530

    port

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get("/", (req, res) => {
   res.send("Ping");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
